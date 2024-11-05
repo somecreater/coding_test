2d57460789cc5239f8b3b09ae983a23cdc8e6e05
somecreater <dbstjrgy123@gmail.com>
2024-11-05 18:15:17 +0900

    섹션 1의 1번문제 정답 해설

--- src/section_1/problem_1.java
+++ src/section_1/problem_1.java
@@ -17,18 +17,33 @@ public class problem_1 {
         return answer;
     }
 
-    public int solution(String str, char c){
+    public int solution(String str, char t){
         int answer=0;
-
+        str=str.toUpperCase();
+        t=Character.toUpperCase(t);
+        //System.out.println(str+" "+t);
+		/*for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)==t) answer++;
+		}*/
+        for(char x : str.toCharArray()){
+            if(x==t) answer++;
+        }
         return answer;
     }
 
     public static void main(String[] args) {
         problem_1 T= new problem_1();
         Scanner sc=new Scanner(System.in);
+
+        /*
         String str=sc.nextLine();
         char ch=sc.next().toUpperCase().toCharArray()[0];
-        System.out.println(T.my_solution(str,ch));
-        //System.out.println(T.solution(str,ch));
+        */
+
+        String str=sc.next();
+        char c=sc.next().charAt(0);
+
+        //System.out.println(T.my_solution(str,ch));
+        System.out.println(T.solution(str,c));
     }
 }
