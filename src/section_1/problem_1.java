be7eee44348c9980198beae42919f630c4124998
somecreater <dbstjrgy123@gmail.com>
2024-11-05 17:51:38 +0900

    섹션 1의 1번문제 풀이 완료(정답 맞춤)

--- src/section_1/problem_1.java
+++ src/section_1/problem_1.java
@@ -3,7 +3,21 @@ package section_1;
 import java.util.Scanner;
 
 public class problem_1 {
-    public int solution(String str){
+
+    public int my_solution(String str, char c){
+        int answer=0;
+        char[] strtoarr=str.toUpperCase().toCharArray();
+
+        for(char obj:strtoarr){
+            if(obj==c){
+                answer+=1;
+            }
+        }
+
+        return answer;
+    }
+
+    public int solution(String str, char c){
         int answer=0;
 
         return answer;
@@ -12,7 +26,9 @@ public class problem_1 {
     public static void main(String[] args) {
         problem_1 T= new problem_1();
         Scanner sc=new Scanner(System.in);
-        String str=sc.next();
-        System.out.println(T.solution(str));
+        String str=sc.nextLine();
+        char ch=sc.next().toUpperCase().toCharArray()[0];
+        System.out.println(T.my_solution(str,ch));
+        //System.out.println(T.solution(str,ch));
     }
 }
