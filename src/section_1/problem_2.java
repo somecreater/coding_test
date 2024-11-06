11a0ba096173fbc9a3be7ca90ee814d3d08cd279
somecreater <dbstjrgy123@gmail.com>
2024-11-06 14:05:37 +0900

    섹션 1의 2번문제 정답 해설

--- src/section_1/problem_2.java
+++ src/section_1/problem_2.java
@@ -18,7 +18,21 @@ public class problem_2 {
     }
 
     public String solution(String str){
-        String answer=new String();
+        String answer="";
+
+        /*
+        //char 배열로 변경하고 직접 확인하여 풀이
+        for(char x : str.toCharArray()){
+            if(Character.isLowerCase(x)) answer+=Character.toUpperCase(x);
+            else answer+=Character.toLowerCase(x);
+
+        }*/
+
+        //아스키 코드를 이용해서 풀이(형변환 반드시 필요), 대문자: 65~90
+        for(char x : str.toCharArray()){
+            if(x>=97 && x<=122) answer+=(char)(x-32);
+            else answer+=(char)(x+32);
+        }
 
         return answer;
     }
@@ -27,8 +41,13 @@ public class problem_2 {
         problem_2 T= new problem_2();
         Scanner sc=new Scanner(System.in);
 
+        /*
         String str=sc.next();
         String answer=new String(T.my_solution(str));
+        */
+
+        String str=sc.next();
+        String answer=T.solution(str);
         System.out.println(answer);
     }
 }
