1abf46f0abd6780fbc52f8c9547a2bcc74e89490
somecreater <dbstjrgy123@gmail.com>
2024-11-09 23:08:28 +0900

    섹션 1의 5번문제 풀이 완료(어떻게 푸는지 모르겠다.)

---
+++ src/section_1/problem_5.java
@@ -0,0 +1,41 @@
+package section_1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class problem_5 {
+
+    public String my_solution(String str){
+        String answer="";
+        char[] charr=str.toCharArray();
+        int fr=0;
+        int ba=charr.length-1;
+        //어떻게 푸는 지 모르겠다.
+        while(fr<ba){
+
+        }
+
+
+        return answer;
+    }
+
+    public String solution(String str){
+        String answer="";
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        problem_5 T= new problem_5();
+        Scanner sc=new Scanner(System.in);
+        String answer="";
+        String str=sc.next();
+
+        answer = T.my_solution(str);
+        System.out.println(answer);
+
+
+
+    }
+}
