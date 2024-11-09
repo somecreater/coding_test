aa8988d725b4220d5995f0afcd609e4b8f534b98
somecreater <dbstjrgy123@gmail.com>
2024-11-09 15:54:50 +0900

    섹션 1의 4번문제 정답 해설

--- src/section_1/problem_4.java
+++ src/section_1/problem_4.java
@@ -1,5 +1,6 @@
 package section_1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -13,15 +14,39 @@ public class problem_4 {
         return answer;
     }
 
-    public String solution(String str){
-        String answer="";
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer=new ArrayList<>();
+        /*
+        StringBuilder의 reverse 메소드를 활용하는 방법
+        for(String x : str){
+            String tmp=new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        */
+
+        //char 배열로 전환해서 직접 요소들의 위치를 바꾸고 나서, String 형으로 변환시키는 방법
+        for(String x : str){
+            char[] s=x.toCharArray();
+            int lt=0, rt=x.length()-1;
+            while(lt<rt){
+                char tmp=s[lt];
+                s[lt]=s[rt];
+                s[rt]=tmp;
+                lt++;
+                rt--;
+            }
+            String tmp=String.valueOf(s);
+            answer.add(tmp);
+        }
 
         return answer;
     }
 
+
     public static void main(String[] args) {
         problem_4 T= new problem_4();
         Scanner sc=new Scanner(System.in);
+        /*
         int N=sc.nextInt();
         String strarr="";
         String[] stranswer=new String[N];
@@ -32,6 +57,16 @@ public class problem_4 {
         }
         for(int j=0;j<N;j++){
             System.out.println(stranswer[j]);
+        }
+        */
+
+        int n=sc.nextInt();
+        String[] str=new String[n];
+        for(int i=0; i<n; i++){
+            str[i]=sc.next();
+        }
+        for(String x : T.solution(n, str)){
+            System.out.println(x);
         }
     }
 }
