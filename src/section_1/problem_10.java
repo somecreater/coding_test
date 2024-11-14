224158815b2e2562d03a31b326c21bf43db7d786
somecreater <dbstjrgy123@gmail.com>
2024-11-14 12:27:21 +0900

    섹션 1의 10번문제 정답 해설

--- src/section_1/problem_10.java
+++ src/section_1/problem_10.java
@@ -25,22 +25,55 @@ public class problem_10 {
         return answer;
     }
 
+
     /*
+       일단 거리 값 p의 초기값을 1000으로 두고 처음에
+       해당 문자가 아니면 1001로 놓는다. 만약 해당 문자를 만나면
+       배열에 넣을 p값을 0으로 놓고 그후로 안만나면 1,2,3, 이런식으로 증가 시키다가
+       해당 문자를 만나면 0으로 놓고 다시 1,2,3 이런식으로 증가 시킨다.
+       이제는 다시 for 문이 거꾸로 돈다.
+       배열에서 0을 만나면 p값을 0으로 두고 0을 만나기 전까지 1,2,3 이런식으로
+       증가 시키지만, 이제는 p값과 배열내의 값을 비교해서 가장 작은 값을 배열에
+       집어 넣는다.
 
     */
-    public String solution(String str){
-        String answer="";
-
+    public int[] solution(String s, char t){
+        int[] answer=new int[s.length()];
+        int p=1000;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==t){
+                p=0;
+                answer[i]=p;
+            }
+            else{
+                p++;
+                answer[i]=p;
+            }
+        }
+        p=1000;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==t) p=0;
+            else{
+                p++;
+                answer[i]=Math.min(answer[i], p);
+            }
+        }
         return answer;
     }
 
 
     public static void main(String[] args) {
-        problem_10 T= new problem_10();
+        problem_10 T= new problem_10();/*
         Scanner sc=new Scanner(System.in);
         String str=sc.next();
         String charstr=sc.next();
         System.out.println(T.my_solution(str,charstr));
-
+        */
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        char c=kb.next().charAt(0);
+        for(int x : T.solution(str, c)){
+            System.out.print(x+" ");
+        }
     }
 }
