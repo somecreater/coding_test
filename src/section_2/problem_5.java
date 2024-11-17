21b5e2647831347fdd91074fdc9ed1154bb45aa3
somecreater <dbstjrgy123@gmail.com>
2024-11-17 21:53:22 +0900

    섹션 2의 5번문제 풀이중

---
+++ src/section_2/problem_5.java
@@ -0,0 +1,31 @@
+package section_2;
+
+import java.util.Scanner;
+
+public class problem_5 {
+
+    /*
+
+    */
+    public int my_solution(int n){
+        int answer=0;
+
+        return answer;
+    }
+
+
+    /*
+
+    */
+    public int solution(int n){
+        int answer=0;
+
+        return answer;
+	}
+
+    public static void main(String[] args) {
+        problem_5 T= new problem_5();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+    }
+}
