9fcdc9d48b7fd17e01190c6a274ac03f94411b1f
somecreater <dbstjrgy123@gmail.com>
2024-11-20 12:44:32 +0900

    섹션 2의 5번문제 풀이 완료(정답 못 맞춤)

--- src/section_2/problem_5.java
+++ src/section_2/problem_5.java
@@ -5,11 +5,25 @@ import java.util.Scanner;
 public class problem_5 {
 
     /*
-
+        일일히 소수 여부를 나눠서 검사하는 방법
+        말고는 판단이 안된다.
     */
     public int my_solution(int n){
         int answer=0;
+        for(int i=2;i<n;i++){
+            boolean isthis=true;
 
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0&&i!=2){
+                    isthis=false;
+                    break;
+                }
+            }
+
+            if(isthis){
+                answer++;
+            }
+        }
         return answer;
     }
 
@@ -27,5 +41,6 @@ public class problem_5 {
         problem_5 T= new problem_5();
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        System.out.println(T.my_solution(n));
     }
 }
