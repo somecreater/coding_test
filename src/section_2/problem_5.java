7c2dabeff2b041257ab2f3e25a2514e0b736239a
somecreater <dbstjrgy123@gmail.com>
2024-11-20 13:42:59 +0900

    섹션 2의 5번문제 정답 해설

--- src/section_2/problem_5.java
+++ src/section_2/problem_5.java
@@ -29,12 +29,23 @@ public class problem_5 {
 
 
     /*
-
+        에라토스테네스 체를 이용해서 푼다.
+        일단 자연수 n을 입력 받는다.
+        길이가 n+1인 배열을 전부 0으로 초기화 한 상태로 생성하고
+        인덱스 2부터 0이면 소수라고 카운팅 하고, 해당 인덱스의 요소를
+        1로 변환하고 해당 인덱스의 배수들도 전부 1로 체크한다
+        이를 계속 반복하면 소수를 구할 수 있다.
     */
     public int solution(int n){
-        int answer=0;
-
-        return answer;
+        int cnt=0;
+        int[] ch = new int[n+1];
+        for(int i=2; i<=n; i++){
+            if(ch[i]==0){
+                cnt++;
+                for(int j=i; j<=n; j=j+i) ch[j]=1;
+            }
+        }
+        return cnt;
 	}
 
     public static void main(String[] args) {
