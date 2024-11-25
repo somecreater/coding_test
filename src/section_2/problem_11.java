3187116c2bde61a695d5a01056f7ae700553cac4
somecreater <dbstjrgy123@gmail.com>
2024-11-25 22:49:50 +0900

    섹션 2의 11번문제 정답 해설

--- src/section_2/problem_11.java
+++ src/section_2/problem_11.java
@@ -35,18 +35,40 @@ public class problem_11 {
 
 
     /*
-
+       int[n+1][6]을 입력으로 받는다.
+       삼중 for 문을 이용 i,j,k
+       i번 학생과 j번 학생의 같은 반여부 비교해서 카운팅
+       k는 1부터 5이다. 즉 학년이다.
+       a[i][k], a[j][k]를 서로비교,
+       그리고 추가로 그 다음 학년에서 같은 경우에 다시 카운팅 하는것은 안된다.(중복 x)
+       그래서 break 를 추가해줘야한다.
     */
 
     public int solution(int n, int[][] arr){
-        int answer=0;
-
+        int answer=0, max=0;
+        for(int i=1; i<=n; i++){
+            int cnt=0;
+            for(int j=1; j<=n; j++){
+                for(int k=1; k<=5; k++){
+                    if(arr[i][k]==arr[j][k]){
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt>max){
+                max=cnt;
+                answer=i;
+            }
+        }
         return answer;
 	}
 
     public static void main(String[] args) {
         problem_11 T= new problem_11();
         Scanner sc=new Scanner(System.in);
+
+        /*
         int n=sc.nextInt();
         int[][] arr=new int[n][n];
         for(int i=0;i<n;i++){
@@ -55,6 +77,15 @@ public class problem_11 {
             }
         }
         System.out.println(T.my_solution(n,arr));
+        */
 
+        int n=sc.nextInt();
+        int[][] arr=new int[n+1][6];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=5; j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.print(T.solution(n, arr));
     }
 }
