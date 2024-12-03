8cb10b3b83d61acaa1ebb54268c7d2f1a0c1d895
somecreater <dbstjrgy123@gmail.com>
2024-12-03 12:00:21 +0900

    섹션 4의 3번문제 정답 해설

--- src/section_4/problem_3.java
+++ src/section_4/problem_3.java
@@ -49,11 +49,32 @@ public class problem_3 {
 
 
     /*
+    자연수 n,k, 그리고 배열 arr을 입력 받는다.
+    인덱스 rt,lt를 이용한다.
 
+    먼저 for문이 k-1바퀴 돌아서 초기 세팅을 하고,
+    그다음에 rt가 돌면서 lt가 쫓아오는 방식으로 푼다.
+    rt가 인덱스 k부터 끝까지 돈다.
+    한 바퀴마다, HashMap에 새로운 값을 추가하거나 그 전의 값을 제거하는
+    방식을 이용한다.(lt, rt 활용) 이 과정을 수행하고 나서 HashMap의 size 메소드를 활용하면
+    값의 종류 개수를 구할 수 있다.
+    특히 뺄때 0이면 키를 삭제해야한다. 더할때 이미 기존에 있는 값이면 해당 키에 대한
+    값만 증가
     */
     public ArrayList<Integer> solution(int n, int k, int[] arr){
-        ArrayList<Integer> answer=new ArrayList<>();
-
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<Integer, Integer> HM = new HashMap<>();
+        for(int i=0; i<k-1; i++){
+            HM.put(arr[i], HM.getOrDefault(arr[i], 0)+1);
+        }
+        int lt=0;
+        for(int rt=k-1; rt<n; rt++){
+            HM.put(arr[rt], HM.getOrDefault(arr[rt], 0)+1);
+            answer.add(HM.size());
+            HM.put(arr[lt], HM.get(arr[lt])-1);
+            if(HM.get(arr[lt])==0) HM.remove(arr[lt]);
+            lt++;
+        }
         return answer;
     }
 
@@ -66,7 +87,8 @@ public class problem_3 {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        ArrayList<Integer> answer=T.my_solution(n,k,arr);
+        //ArrayList<Integer> answer=T.my_solution(n,k,arr);
+        ArrayList<Integer> answer=T.solution(n,k,arr);
         for(int a:answer){
             System.out.print(a+" ");
         }
