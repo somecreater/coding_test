1cbceefac19dc2f2e1bd0a2906946261ffc1b7a1
somecreater <dbstjrgy123@gmail.com>
2024-12-16 15:14:35 +0900

    섹션 5의 6번문제 풀이 중

---
+++ src/section_5/problem_6.java
@@ -0,0 +1,33 @@
+package section_5;
+
+import java.util.Scanner;
+
+public class problem_6 {
+
+    /*
+
+    */
+    public int my_solution(int N,int K){
+        int answer=0;
+
+        return answer;
+    }
+
+    /*
+
+    */
+    public int solution(int N,int K){
+        int cnt=0;
+
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        problem_6 T= new problem_6();
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int K=sc.nextInt();
+        System.out.println(T.my_solution(N,K));
+        //System.out.println(T.solution(N,k));
+    }
+}
