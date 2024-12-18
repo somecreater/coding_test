798058a3422039c4ebe90e73d01234b37558053c
somecreater <dbstjrgy123@gmail.com>
2024-12-18 13:16:37 +0900

    섹션 6의 1번문제 풀이 완료(정답 맞춤)

--- src/section_6/problem_1.java
+++ src/section_6/problem_1.java
@@ -3,8 +3,37 @@ package section_6;
 import java.util.Scanner;
 
 public class problem_1 {
-    public int solution(String str){
-        int answer=0;
+
+    /*
+    자연수 N, 자연수 배열 arr 을 입력받는다
+    그리고 이중 for 문을 이용해서 arr 에서 최소값을 찾아서
+    answer 배열에 추가, 그에 맞게 arr 의 요소들의 실제 위치를 변경한다.
+    그리고, 인덱스 i를 증가시키고 그 이후에서 최솟값을 찾는
+    방식으로 풀면된다.
+    */
+    public int[] my_solution(int N,int[] arr){
+        int[] answer=new int[N];
+        int min=0;
+        for(int i=0;i<N;i++){
+            min=100;
+            for(int j=i;j<N;j++){
+                if(min>arr[j]){
+                    min=arr[j];
+                    int tmp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=tmp;
+                }
+            }
+            answer[i]=min;
+        }
+        return answer;
+    }
+
+    /*
+
+    */
+    public int[] solution(int N,int[] arr){
+        int[] answer=new int[N];
 
         return answer;
     }
@@ -12,7 +41,13 @@ public class problem_1 {
     public static void main(String[] args) {
         problem_1 T= new problem_1();
         Scanner sc=new Scanner(System.in);
-        String str=sc.next();
-        System.out.println(T.solution(str));
+        int N=sc.nextInt();
+        int[] arr=new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int a:T.my_solution(N,arr)){
+            System.out.print(a+" ");
+        }
     }
 }
