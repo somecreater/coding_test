acad2df015853e1aa460cf82f67619f0f51eb111
somecreater <dbstjrgy123@gmail.com>
2024-12-18 16:49:19 +0900

    섹션 6의 3번문제 정답 해설

--- src/section_6/problem_3.java
+++ src/section_6/problem_3.java
@@ -27,10 +27,22 @@ public class problem_3 {
     }
 
     /*
-
+    이것도 이중 for 문을 이용한다.
+    인덱스 i, j 를 이용하고 i 는 1 부터 n 까지 j 는 i-1 부터 0까지 돈다.
+    tmp 초기값은 arr[i] 값을 넣고 만약 tmp 값보다 arr[j] 가 크다면
+    arr[j+1]에 arr[j] 값을 넣는다. 만약 조건을 만족하지 않는다면
+    자동으로 멈춘다.
+    그리고 arr[j+1]에 tmp 값을 넣는다(뒤로 민다고 생각)
     */
     public int[] solution(int n, int[] arr){
-
+        for(int i=1; i<n; i++){
+            int tmp=arr[i], j;
+            for(j=i-1; j>=0; j--){
+                if(arr[j]>tmp) arr[j+1]=arr[j];
+                else break;
+            }
+            arr[j+1]=tmp;
+        }
         return arr;
     }
 
@@ -43,14 +55,14 @@ public class problem_3 {
             arr[i]=sc.nextInt();
         }
 
-
+/*
         for(int a:T.my_solution(n,arr)){
             System.out.print(a+" ");
         }
-        /*
+ */
         for(int a:T.solution(n,arr)){
             System.out.print(a+" ");
         }
-        */
+
     }
 }
