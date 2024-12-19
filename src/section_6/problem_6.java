e185ebb3e1e1113010ec63cbf9ed7721abc76fac
somecreater <dbstjrgy123@gmail.com>
2024-12-19 16:03:08 +0900

    섹션 6의 6번문제 정답 해설

--- src/section_6/problem_6.java
+++ src/section_6/problem_6.java
@@ -1,5 +1,7 @@
 package section_6;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class problem_6 {
@@ -29,11 +31,16 @@ public class problem_6 {
     }
 
     /*
-
+    이 문제는 원래 순서로 정렬된 배열과 입력된 배열을 비교해서 바뀐
+    부분을 확인하면 되는 문제이다.
     */
-    public int[] solution(int n,int[] arr){
-        int[] answer=new int[2];
-
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer=new ArrayList<>();
+        int[] tmp=arr.clone();
+        Arrays.sort(tmp);
+        for(int i=0; i<n; i++){
+            if(arr[i]!=tmp[i]) answer.add(i+1);
+        }
         return answer;
     }
 
@@ -46,9 +53,12 @@ public class problem_6 {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
+        /*
         for(int a:T.my_solution(n,arr)){
             System.out.print(a+" ");
         }
-
+        */
+        for(int x : T.solution(n, arr))
+            System.out.print(x+" ");
     }
 }
