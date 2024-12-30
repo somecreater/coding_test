2ff2d0de572a3d140c3c7e3d0324bb488eb5d385
somecreater <dbstjrgy123@gmail.com>
2024-12-30 19:20:25 +0900

    섹션 6의 10번 문제 정답 해설

--- src/section_6/problem_10.java
+++ src/section_6/problem_10.java
@@ -55,12 +55,42 @@ public class problem_10 {
     }
 
     /*
-
+        일단 수직선 상에 마구간을 표현한다.
+        두 말의 최소, 최대가능 거리를 각각 lt, rt로 잡고 일단 가장 가까운 두 점 거리와
+        가장 먼 두점 거리(아니면 가장 큰 좌표값)를 초기값으로 설정한다.
+        그중에서 (lt+rt)/2를 mid 값을 가장 가까운 거리라고 가정하고 말의 최대 배치 수를 계산한다
+        만약 최대 배치수가 주어진 말보다 훨씬 많거나 같다면 mid-1~rt 범위에 대해
+        lt, rt, mid 를 다시 잡고 같은 연산을 실행한다.
+        적다면 lt~mid-1 범위에 대해 lt, rt, mid 를 다시 잡고 같은 연산을 실행하는 방식으로
+        좁혀나가면 된다.
+        arr[i]-ep(end position) 조건 이용
+        결국 최적의 답을 찾아간다.
     */
+    public int count(int[] arr, int dist){
+        int cnt=1;
+        int ep=arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]-ep>=dist){
+                cnt++;
+                ep=arr[i];
+            }
+        }
+        return cnt;
+    }
 
-    public int solution(int n, int m, int[] arr){
+    public int solution(int n, int c, int[] arr){
         int answer=0;
-
+        Arrays.sort(arr);
+        int lt=1;
+        int rt=arr[n-1];
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+            if(count(arr, mid)>=c){
+                answer=mid;
+                lt=mid+1;
+            }
+            else rt=mid-1;
+        }
         return answer;
     }
 
@@ -74,7 +104,7 @@ public class problem_10 {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        System.out.println(T.my_solution(n,m,arr));
-        //System.out.println(T.solution(n,m,arr));
+        //System.out.println(T.my_solution(n,m,arr));
+        System.out.println(T.solution(n,m,arr));
     }
 }
