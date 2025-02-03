20ed48fa6c673c08971fa3d5459e27681c0bc677
somecreater <dbstjrgy123@gmail.com>
2025-02-03 17:55:03 +0900

    섹션 7의 8번문제 정답 해설

--- src/section_7/problem_1.java
+++ src/section_7/problem_1.java
@@ -224,15 +224,66 @@ public class problem_1 {
 
         }
     }
+    /*
+        현수는 스카이 콩콩으로 -1,1,5 만큼의 거리를 이동할 수 있다.
+        ex) 5(0 Level) -> 4 6 10(1 Level)-> 7 11 3 9 15(2 Level)
+        뭔가 check 배열을 만들어서 안간 곳만 가도록 해야한다
+        예시와 같이 전개하다가 송아지 위치랑 같아지면 해당 함수를 중단한다.
+
+        즉, 송아지 위치에 도달하기 전까지 위치목록이 저장된 q에서 한개씩 값을
+        가져와서 -1,1,5 경우를 안가본곳 한정으로 순차적으로
+        q에 해당 경우를 넣으면서 답을 찾으면 된다.(q는 뒤에 값을 추가하므로
+        섞이는 경우는 없다)
+
+        이때 레벨은 초기 길이를 구하고 해당 길이만큼의 계산이 끝나면
+        증가 시키면 된다(0레벨, 1레벨, ...)
+
+        ch<- 체크 배열
+        dis<- -1,1,5 이동 선택지
+        q<- 현재 가있는 위치 목록들
+
+    */
+    int answer=0;
+    int[] dis={1, -1, 5};
+    int[] ch_2;
+    Queue<Integer> Q = new LinkedList<>();
+    public int solution_7(int s, int e){
+        ch_2=new int[10001];
+        ch_2[s]=1;
+        Q.offer(s);
+        int L=0;
+        while(!Q.isEmpty()){
+            int len=Q.size();
+            for(int i=0;i<len;i++){
+                int x=Q.poll();
+                for(int j=0;j<3;j++){
+                    int nx=x+dis[j];
+                    if(nx==e) return L+1;
+                    if(nx>=1&&nx<=10000&&ch_2[nx]==0){
+                        ch_2[nx]=0;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
         problem_1 T= new problem_1();
         Scanner sc=new Scanner(System.in);
+        /*
         s=sc.nextInt();
         e=sc.nextInt();
         s-=1;
         e-=1;
         T.my_solution_7(movecnt,s);
+         */
+        int s=sc.nextInt();
+        int e=sc.nextInt();
+        System.out.println(T.solution_7(s,e));
+
     }
 }
 
